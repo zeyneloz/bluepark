0f1a5a707671b7ce8c0f8cfed4c3d0ae2af53af0
zeyneloz <zeynel.ozdmir@gmail.com>
2018-11-29 01:32:08 +0100

    Introduce custom types

---
+++ bluepark/types.py
@@ -0,0 +1,8 @@
+import typing
+
+ASGIScope = typing.Mapping[str, typing.Any]
+ASGIMessage = typing.Dict[str, typing.Any]
+ASGIReceive = typing.Callable[[], typing.Awaitable[ASGIMessage]]
+ASGISend = typing.Callable[[ASGIMessage], typing.Awaitable[None]]
+ASGIAppInstance = typing.Callable[[ASGIReceive, ASGISend], typing.Awaitable[None]]
+ASGIApp = typing.Callable[[ASGIScope], ASGIAppInstance]
