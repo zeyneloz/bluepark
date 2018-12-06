156fa6247baf31472a716d8160007904fb27e25f
zeyneloz <zeynel.ozdmir@gmail.com>
2018-12-06 20:26:54 +0100

    Introduce new errors

--- bluepark/exceptions.py
+++ bluepark/exceptions.py
@@ -1,4 +1,14 @@
 
 class RequestBodyNotExist(Exception):
-    """Http request body is not yet received"""
+    '''Http request body is not yet received'''
+    pass
+
+
+class HTTPConnectionClosed(Exception):
+    '''HTTP connection is closed or if receive is called after a response has been sent'''
+    pass
+
+
+class BodyAlreadyReceived(Exception):
+    '''All of the HTTP body is already received'''
     pass
